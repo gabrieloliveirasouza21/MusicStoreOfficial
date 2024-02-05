fcc927c902f65f0e4b07771e3b28f7f165aae667
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2024-02-05 10:52:11 -0300

    updated

---
+++ MusicStoreOfficial/MusicStoreOfficial.Client/ViewModel/DeleteAlbumViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using MvvmBlazor.ViewModel;
+using SharedLibrary.Models;
+using SharedLibrary.Repositories;
+
+namespace MusicStoreOfficial.Client.ViewModel
+{
+    public class DeleteAlbumViewModel : ViewModelBase
+    {
+        [Parameter]
+        public int Id { get; set; }
+        public string isDeleted = "";
+
+
+        private readonly IAlbumRepository _albumRepository;
+
+        public DeleteAlbumViewModel(IAlbumRepository albumRepository)
+        {
+            _albumRepository = albumRepository;
+        }
+
+        public async Task<Album> DeleteAlbum()
+        {
+            var delete = await _albumRepository.DeleteAlbumAsync(Id);
+            isDeleted = "Album deletado com sucesso";
+            await Task.Delay(500);
+            return delete;
+
+        }
+    }
+}
